d426c3f0f4ae2c506faf97dfcc42feed2bcc5c29
Abdyl-Aziz <azizshihabov@mail.ru>
2021-07-16 22:46:16 +0200

    feat: added bg js project

---
+++ BackgroundChanger/js/main.js
@@ -0,0 +1,45 @@
+const btn = document.getElementById('btn');
+const color = document.getElementById('background');
+
+btn.addEventListener('click', () => {
+  let Color = generateHex();
+  document.body.style.backgroundColor = Color;
+  color.innerHTML = Color;
+
+
+});
+
+const hex = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+]
+
+function generateHex(){
+  let hexColor = '#';
+
+  for(let i = 0; i < 6; i++){
+    hexColor += hex[getRandomNumber()];
+  }
+  
+  return hexColor;
+}
+
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+
+}
